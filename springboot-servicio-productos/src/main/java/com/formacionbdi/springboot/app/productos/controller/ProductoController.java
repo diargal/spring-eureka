e841643c7bc0e791dc663f52f54f46a60298f213
diearmga <diearmga@bancolombia.com.co>
2022-08-11 16:22:27 -0500

    Balanceo de carga con Ribbon y RestTemplate

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controller/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controller/ProductoController.java
@@ -3,13 +3,13 @@ package com.formacionbdi.springboot.app.productos.controller;
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
 import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
@@ -18,14 +18,19 @@ public class ProductoController {
     @Autowired
     private IProductoService iProductoService;
 
+    //Se puede obtener el puerto mediante Environment o por @Value
     @Autowired
     private Environment environment;
+
+    @Value("${server.port}")
+    private Integer port;
 
     @GetMapping("/listar")
     public List<Producto> listar() {
         return iProductoService.findAll().stream().map(
                         producto -> {
-                            producto.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+                            // producto.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+                            producto.setPort(port);
                             return producto;
                         }
                 )
@@ -36,7 +41,8 @@ public class ProductoController {
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id) {
         Producto producto = iProductoService.findById(id);
-        producto.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+        // producto.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+        producto.setPort(port);
 
         return producto;
     }
