b11e52acde4d07a42a4afc14c2ed90890d7a5ab6
diego_garcia <digarciaalt@gmail.com>
2023-05-26 16:29:34 -0500

    Se crean filtros pre y post

---
+++ springboot-zuul-server-spring/src/main/java/com/springboot/formacionbdi/springbootzuulserverspring/filters/PostTiempoTranscurridoFilter.java
@@ -0,0 +1,46 @@
+package com.springboot.formacionbdi.springbootzuulserverspring.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class PostTiempoTranscurridoFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(PreTiempoTranscurridoFilter.class);
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        log.info("Entrando al post filter");
+
+        Long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
+        Long tiempoFinal = System.currentTimeMillis();
+        Long tiempoTranscurrido = tiempoFinal - tiempoInicio;
+
+        log.info(String.format("El tiempo transcurrido es %s segundos: ", tiempoTranscurrido.doubleValue() / 1000));
+
+        return null;
+    }
+}
